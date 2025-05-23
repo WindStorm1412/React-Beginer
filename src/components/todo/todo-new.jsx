f4931a500143213c777e8e607ad5e2881b9a718c
WindStorm <nguyenduongb2004@gmail.com>
2025-05-23 21:56:51 +0700

    video 36

--- src/components/todo/todo-new.jsx
+++ src/components/todo/todo-new.jsx
@@ -1,10 +1,21 @@
 const TodoNew = (props) => {
- const {addNewTodo}=props
-//  addNewTodo("WindStorm")
+    const { addNewTodo } = props
+    //  addNewTodo("WindStorm")
+    const handleClick = () => {
+        alert("click me")
+    }
+    const handleOnchange=(name)=>{
+        console.log("WindStorm",name)
+    }
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" 
+            onChange={(event)=>{handleOnchange(event.target.value)} }
+            />
+            <button 
+            style={{cursor:"pointer"}}
+            onClick={handleClick}
+            >Add</button>
         </div>
     )
 
