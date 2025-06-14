4b23a4301a49d8eaf2cdc47e6e97106162f8604f
WindStorm <nguyenduongb2004@gmail.com>
2025-06-14 19:51:36 +0700

    video 89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,56 @@
+import { Button, Form, Input } from "antd"
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
     return (
         <>
-            <div>register</div>
+
+            <div style={{ display: "flex", flexDirection: "column", margin: "50px", width: "30%", }}>
+                <Form
+                    onFinish={onFinish}
+                    // onFinishFailed={onFinishFailed}
+                    layout="vertical"
+                    form={form}
+                >
+                    <Form.Item
+                        label="Full Name"
+                        name="fullName"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                    <Form.Item
+                        label="Phone Number"
+                        name="phone"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <div >
+                        <Button
+                            onClick={() => form.submit()}
+                            type="primary">Register</Button>
+                    </div>
+                </Form>
+            </div>
+
         </>
     )
 }
